3b186f27e826333c4457257ead1f826d888e38d4
Чачка Пипсов <161365869+x2latte@users.noreply.github.com>
2026-02-27 23:57:57 +0300

    FIxed (core + models + routes + schemas)

---
+++ core/config.py
@@ -0,0 +1,6 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str = "Internet Shop"
+
+settings = Settings()
---
+++ core/security.py
@@ -0,0 +1,4 @@
+# Заготовка под JWT авторизацию
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
